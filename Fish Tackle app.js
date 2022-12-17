ca465119509c1866b6ebeed68faebaf8a4f455f7
Leonardo Hobeni <www.bongahobeni2@gmail.com>
2022-12-17 21:24:39 +0200

    Added buttons and operations

---
+++ Fish Tackle app.js
@@ -0,0 +1,130 @@
+var mainPage; //variable to hold the reference to an html container
+var activeItem;//variable to hold the reference to an item instance
+var dataStore=[];//array variable to hold the item objects
+var stringDataStore=[];//array variable to hold the stringified item objects
+var jsonString;//variable to hold the stringified stringDataStore array
+var storeName;
+const EMPTY_STORE=0, INVALID_STORE=1, LOAD_STORE_OK=2;//signal values or flag values
+
+function doStartApp(id, name)
+{
+    storeName= name;
+    mainPage= document.getElementById(id);
+    doShowMenu();
+}
+
+function openPage(title)
+{
+    clearPage(mainPage);
+    var titlePar= document.createElement('p');
+    titlePar.className='mainTitle';
+    titlePar.innerText=title;
+    mainPage.appendChild(titlePar);
+}
+
+function clearPage(containerElement)
+{
+    while(containerElement.children.length > 0)
+        containerElement.removeChild(containerElement.children[0]);
+}
+
+function doShowMenu()
+{
+    openPage('Main Menu');
+    
+    showMenu(
+        [
+            {capt: 'Add Aerosol', label: 'Aerosol', func: 'doAddAerosol()'},
+            {capt: 'Add Backpack', label: 'Backpack', func: 'doAddBackPack()'},
+            {capt: 'Update Stock', label: 'Update', func: 'doUpdateStock()'},
+            {capt: 'List stock items', label: 'List', func: 'doListStockItems()'},
+            {capt: 'Data Analysis', label: 'Analysis', func: 'displayDataAnalysisMenu()'},
+        ]
+    );
+}
+
+function doUpdateStock()
+{
+    openPage("Update Stock");
+    
+    //create reference input panel
+    var inputPar= document.createElement('p');
+
+    //create label
+    var inpLabel= document.createElement('label');
+    inpLabel.className= "inputLabel";
+    inpLabel.innerText= "Reference:";
+    inpLabel.setAttribute('for', 'reference');
+    inputPar.appendChild(inpLabel);
+
+    //create input element
+    var inputElement= document.createElement('input');
+    inputElement.className='inputText';
+    inputElement.setAttribute('id', 'reference');
+    inputPar.appendChild(inputElement);
+    mainPage.appendChild(inputPar);
+    showMenu(
+        [
+            {capt: 'Find item', label: 'Find', func: 'doFindItem()'},
+            {capt: 'Cancel update', label: 'Cancel', func: 'doCancelUpdate()'},
+        ]
+    );
+}
+
+function doCancelUpdate()
+{
+    doShowMenu();
+}
+
+function doAddAerosol()
+{
+    addStockItem(Aerosol);
+}
+
+function doAddBackPack()
+{
+    addStockItem(Backpack);
+}
+
+function addStockItem(StockItem)
+{
+    activeItem= new StockItem();
+    openPage('Add '+activeItem.type);
+    activeItem.getHTML(mainPage);
+    showMenu(
+        [
+            {capt: 'Save Item', label: 'Save', func: 'doSaveAdd()'},
+            {capt: 'Cancel Item', label: 'Cancel', func: 'doShowMenu()'},
+        ]
+    );
+}
+
+function showMenu(schema)
+{
+    for(let item of schema)
+    {
+        var itemElement= createOperationButton(item);
+        mainPage.appendChild(itemElement);
+    }
+}
+
+function createOperationButton(item)
+{
+    var btnPar= document.createElement('p');
+
+    //create label
+    var btnLabel= document.createElement('label');
+    btnLabel.className='mainLabel';
+    btnLabel.innerText= item.capt;
+    btnLabel.setAttribute('for', item.label);
+    btnPar.appendChild(btnLabel);
+
+    //create button
+    var btnElement= document.createElement('button');
+    btnElement.className= 'mainButton';
+    btnElement.innerText= item.label;
+    btnElement.setAttribute('onclick', item.func);
+    btnPar.appendChild(btnElement);
+
+    return btnPar;
+}
---
+++ StockItems.js
@@ -0,0 +1,145 @@
+class StockItems{
+
+    constructor(stockLevel, stockRef, price, brand, description, packageWeight)
+    {
+        this.stockLevel= stockLevel;
+        this.stockRef= stockRef;
+        this.price= price;
+        this.brand= brand;
+        this.description= description;
+        this.packageWeight= packageWeight;
+    }
+
+    getDescription()
+    {
+        var result= "Ref:"+this.stockRef
+            +" Price:"+this.price
+            +" Description:"+this.description
+            +" Brand:"+this.brand
+            +" Stock:"+this.stockLevel
+            +" Weight:"+this.packageWeight;
+        return result;
+    }
+
+    static getLargestStockRef(items)
+    {
+        if(items.length == 0)
+            return 0;
+
+        var largest= items[0].stockRef;
+
+        for(let item of items)
+            if(item.stockRef > largest)
+                largest= item.stockRef;
+        return largest;
+    }
+
+    static stockSchema= [
+        {id: 'price', prompt: 'Price', type: 'input'},
+        {id: 'stockLevel', prompt: 'Stock level', type: 'input'},
+        {id: 'description', prompt: 'Description', type: 'textarea', rows:'4', cols:'30'},
+        {id: 'brand', prompt: 'Brand', type: 'input'},
+        {id: 'packageWeight', prompt: 'Weight', type: 'input'},
+    ];
+
+    static makeItemElement(item)
+    {
+        var inputPar= document.createElement('p');
+
+        //create label element
+        var inpLabel= document.createElement('label');
+        inpLabel.className="inputLabel";
+        inpLabel.innerText= item.prompt+":";
+        inpLabel.setAttribute('for', item.id);
+        inputPar.appendChild(inpLabel);
+
+        //create input element
+        var inpElement;
+        switch(item.type)
+        {
+            case 'input':
+                inpElement= document.createElement('input');
+                inpElement.className= 'inputText';
+                break;
+            case 'textarea':
+                inpElement= document.createElement('textarea');
+                inpElement.className='inputTextArea';
+                break;
+        }
+
+        inpElement.setAttribute('id', item.id);
+        inputPar.appendChild(inpElement);
+        return inputPar;
+    }
+
+    static buildHTMLStockELement(containerElement, dataSchema)
+    {
+        for(let item of dataSchema)
+        {
+            var itemElement= StockItems.makeItemElement(item);
+            containerElement.appendChild(itemElement);
+        }
+    }
+
+    getHTML(containerElement)
+    {
+        StockItems.buildHTMLStockELement(containerElement, StockItems.stockSchema);
+    }
+}
+
+class Aerosol extends StockItems{
+    constructor(stockLevel, stockRef, price, brand, description, packageWeight,
+        scent)
+    {
+        super(stockLevel, stockRef, price, brand, description, packageWeight);
+        this.type='aerosol';
+        this.scent=scent;
+    }
+
+    getDescription()
+    {
+        var result= super.getDescription()
+            +" Scent:"+this.scent;
+        return result;
+    }
+
+    static aerosolSchema= [
+        {id: 'scent', prompt: 'Scent', type: 'input'}
+    ];
+
+    getHTML(containerElement)
+    {
+        super.getHTML(containerElement);
+        StockItems.buildHTMLStockELement(containerElement, Aerosol.aerosolSchema);
+    }
+}
+
+class Backpack extends StockItems{
+    constructor(stockLevel, stockRef, price, brand, description, packageWeight,
+        design, color)
+    {
+        super(stockLevel, stockRef, price, brand, description, packageWeight);
+        this.type='backpack';
+        this.design=design;
+        this.color=color;
+    }
+
+    getDescription()
+    {
+        var result= super.getDescription()
+            +" Design:"+this.design
+            +" Color:"+this.color;
+        return result;
+    }
+
+    static backpackSchema= [
+        {id: 'design', prompt: 'Design', type: 'input'},
+        {id: 'color', prompt: 'Color', type: 'input'},
+    ];
+
+    getHTML(containerElement)
+    {
+        super.getHTML(containerElement);
+        StockItems.buildHTMLStockELement(containerElement, Backpack.backpackSchema);
+    }
+}
